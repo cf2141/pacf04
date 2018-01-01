f5aa04a4b96e050c1096f15aaf6d49cfeb9f8572
cf2141 <cf2141@tgndomains.com>
2018-01-01 18:46:55 -0500

    parse a csv file - working - pacf

--- src/main/java/com/acme/rnd/App.java
+++ src/main/java/com/acme/rnd/App.java
@@ -11,22 +11,68 @@ import java.util.ArrayList;
 import java.util.List;
 
 import java.util.Random;
+import java.util.Scanner;
 
 
 public class App {
   public static void main( String[] args ) {
     System.out.println( "find the file" );
     String localDir = System.getProperty("user.dir"); 
+    String csvFile = null;
+    
     System.out.println("Working Directory = " + localDir );
     File folder = new File( localDir );
     File[] listOfFiles = folder.listFiles();
 
     for (int i = 0; i < listOfFiles.length; i++) {
       if (listOfFiles[i].isFile()) {
-        System.out.println("File " + listOfFiles[i].getName());
+        if (listOfFiles[i].getName().contains(".csv")){
+          System.out.println("File " + listOfFiles[i].getName());
+          csvFile = listOfFiles[i].getName();
+        }
       } else if (listOfFiles[i].isDirectory()) {
       }
     }
+
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+    try {
+      System.out.println("new Scanner(new File( "+csvFile+" ))");
+      Scanner scanner = new Scanner(new File(csvFile));
+      scanner.useDelimiter(",");
+      while(scanner.hasNext()){
+          System.out.print(scanner.next()+"|");
+      }
+      scanner.close();    
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    
+    
+
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+    BufferedReader br = null;
+    String line = "";
+    String cvsSplitBy = ",";
+    try {
+      System.out.println("BufferedReader(new FileReader( "+csvFile+" ))");
+      br = new BufferedReader(new FileReader(csvFile));
+      while ((line = br.readLine()) != null) {
+        // use comma as separator
+        String[] country = line.split(cvsSplitBy);
+        //System.out.println("Country [code= " + country[4] + " , name=" + country[5] + "]");
+      }
+    } catch (IOException e) {
+        e.printStackTrace();
+    } finally {
+      if (br != null) {
+        try {
+          br.close();
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+    }  
+  
   }
 }
 
