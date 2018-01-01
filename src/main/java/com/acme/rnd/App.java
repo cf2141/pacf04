c9add1e349a8142953465697f71f623b2224d1f7
cf2141 <cf2141@tgndomains.com>
2018-01-01 17:45:13 -0500

    list files

--- src/main/java/com/acme/rnd/App.java
+++ src/main/java/com/acme/rnd/App.java
@@ -1,13 +1,32 @@
 package com.acme.rnd;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import java.util.Random;
+
+
+public class App {
+  public static void main( String[] args ) {
+    System.out.println( "find the file" );
+    String localDir = System.getProperty("user.dir"); 
+    System.out.println("Working Directory = " + localDir );
+    File folder = new File( localDir );
+    File[] listOfFiles = folder.listFiles();
+
+    for (int i = 0; i < listOfFiles.length; i++) {
+      if (listOfFiles[i].isFile()) {
+        System.out.println("File " + listOfFiles[i].getName());
+      } else if (listOfFiles[i].isDirectory()) {
+      }
     }
+  }
 }
+
